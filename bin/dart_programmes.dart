c4a2adb461bc47605a932248a2e70b1d944ff3db
harshana <tharshana@wallpostdev.com>
2023-08-31 12:13:28 +0530

    Initial commit

---
+++ bin/dart_programmes.dart
@@ -0,0 +1,5 @@
+import 'package:dart_programmes/dart_programmes.dart' as dart_programmes;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_programmes.calculate()}!');
+}
---
+++ test/dart_programmes_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_programmes/dart_programmes.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
