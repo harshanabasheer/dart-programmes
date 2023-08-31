26ff4a551b56d1100f6152669e5cfc5a8c9d25d9
harshana <tharshana@wallpostdev.com>
2023-08-31 13:05:04 +0530

    list operations

--- lib/String/string.dart
+++ lib/String/string.dart
@@ -4,6 +4,7 @@ void main(){
   print(" length of first string ${str1.length}");//length
   print(" length of second string ${str2.length}");
   String con = str1+str2;
+  print(con);
   print("The concatenated string ${str1} and ${str2}: ${con}");//concatination
   print("str1.compareTo(str2): ${str1.compareTo(str2)==0}");//compare
   print("New String: ${str1.replaceAll('harshana','abdu')}");//replace word
--- lib/list/list.dart
+++ lib/list/list.dart
@@ -1,3 +1,36 @@
 void main(){
-  List num=[]
+  List num=[1,2,3,4,5,6,7,8];
+  print(num);
+  print("Insert the element  INDIA to 1st index position.");
+  num.insert(1,"india");//insert in 1st index
+   print(num);
+   print("-------------------------------------------------");
+  print("Insert 2 elements to the list (starting index :4)");
+  num.insertAll(4,[10,100] );//insert from 4 index
+  print(num);
+  print("-------------------------------------------------");
+
+  print("Remove element from last");
+  print(num.removeLast());
+  print("-------------------------------------------------");
+
+  print("specified index");
+  print(num.removeAt(0));
+  print("-------------------------------------------------");
+  print("remove elements in range");
+  num.removeRange(3, 4);
+  print(num);
+  print("-------------------------------------------------");
+  print("Perform replace function (Starting index:3 , ending index :5 )");
+  num.replaceRange(3, 5, [33,44,55]);
+  print(num);
+  print("-------------------------------------------------");
+
+
+
+
+
+
+
+
 }
